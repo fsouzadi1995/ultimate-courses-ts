22fa1ed0288dced8b15780b5551f8365dc10381e
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-01-02 00:06:54 -0300

    Playing around

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 console.log('Greetings 🧙');
 console.log('Spell!');
+
+const body = document.querySelector('body');
+const div = document.createElement('div');
+
+div.innerHTML = 'peepee';
+div.style.fontFamily = 'Roboto mono, sans-serif';
+div.style.display = 'flex';
+div.style.justifyContent = 'center';
+
+body?.appendChild(div);
