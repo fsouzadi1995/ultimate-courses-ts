1ea0c3b844eddb66c59da7e15500746897232609
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-01-02 19:20:22 -0300

    Never, null, undefined and strick null checks

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,13 @@ console.log('Spell!');
  *  Implicit vs Explicit types
  */
 
-let something; /* Since something is declared without a type nor a value it has a
-                  inferred type of Any */
+let something; // no type supplied nor value assigned means something is inferred of type any
 
-something = true;
+something = true; // valid because of type any
 
-something = 15;
+something = 15; // valid because of type any
 
-something = 'I am something';
+something = 'I am something'; // valid because of type any
 
 let implicitTyping = 'pizza'; // implicitTyping now has implicited that it's a string
 let explicitTyping: string = 'pizzaDiscount'; // explicitTyping has been declared of type string
@@ -20,5 +19,25 @@ let explicitTyping: string = 'pizzaDiscount'; // explicitTyping has been declare
 let alsoExplicit: string; // alsoExplicit is also of explicit type string although unassigned
 
 /**
- *
+ * Never type
  */
+
+// Can be used when our function never returns something or when an error is thrown
+function throwError(err: string): never {
+  throw new Error(err);
+  // never going to return a value
+}
+
+/**
+ * Null, undefined and strict null checks
+ */
+
+// let coupon: string = 'pizza25'; => this coupon cannot be assigned null
+let coupon: string | null = 'pizza25';
+
+// Impure function
+function removeCoupon() {
+  // coupon = null; => this is invalid due to the fact that coupon cannot be of type null
+  // coupon = undefined; => also invalid due to the fact that coupon cannot be of type undefined
+  coupon = null;
+}
